e78b1a52d890b0c92ecc647f214790f0ff197a43
MRomano26 <michael.mar26@gmail.com>
2021-11-06 15:56:38 -0400

    Solution to exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    addString = string;
+    if (num === 0) {
+        return "";
+    }
+    else if (num === 1) {
+        return string;
+    }
+    else if (num < 0) {
+        return "ERROR";
+    }
+    else {
+        for (i = 0; i < num - 1; i++) {
+            string += addString;
+        }
+    }
+    return string;
 };
 
 // Do not edit below this line
