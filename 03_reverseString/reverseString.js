03b452df1f46ef09fa8a6e1060e187b1d7b55461
MRomano26 <michael.mar26@gmail.com>
2021-11-06 16:13:24 -0400

    Solution to exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
 const reverseString = function(str) {
-
+    let strArray = str.split("");
+    let strReverse = "";
+    for (i = strArray.length - 1; i >= 0; i--) {
+        strReverse += strArray[i];
+    }
+    return strReverse;
 };
-
+console.log(reverseString("Hello"));
 // Do not edit below this line
 module.exports = reverseString;
