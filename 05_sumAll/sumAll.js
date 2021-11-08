7cd8b4682a19c0efad59e450b15bf286c5ac537c
MRomano26 <michael.mar26@gmail.com>
2021-11-08 14:21:22 -0500

    Solution to exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let sum = 0;
+    if (typeof x !== "number" || typeof y !== "number") {
+        return "ERROR";
+    }
+    else if ( x < 0 || y < 0) {
+        return "ERROR"
+    }
+    else if (x < y) {
+        for (i = x; i <= y; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    else if (y < x) {
+        for (i = y; i <= x; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    else {
+        //when x = y just return x
+        return x;
+    }
 };
 
 // Do not edit below this line
